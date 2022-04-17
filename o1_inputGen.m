40d714850c43abc06cb39bb8a79cb93f0769c503
eg <cqu>
2022-04-17 15:28:21 +0800

    add optimization to inputGen

--- o1_inputGen.m
+++ o1_inputGen.m
@@ -13,7 +13,12 @@ for i = 1:32
     Input.Marginals(i).Parameters = [rho_1000, k, 0, rho_1000];
 end
 
-Input.Marginals(1).Parameters = [rho_1000, 0.19, 0, 0.5 * rho_1000];
+opt = 0;
+
+if (opt ~= 0)
+    Input.Marginals(opt).Parameters = [rho_1000, 0.19, 0, 0.5 * rho_1000];
+end
+
 myInput = uq_createInput(Input);
 
 sample = 10000;
@@ -21,4 +26,4 @@ input_rho = uq_getSample(myInput, sample, 'Sobol');
 
 k_split = split(num2str(k), '.');
 k_split = k_split{2};
-save(['.\DIM-master\input_k_', num2str(k_split), '_rho1000_', num2str(rho_1000), '_sobol_', num2str(sample)], 'input_rho')
+save(['.\DIM-master\input_k_', num2str(k_split), '_rho1000_', num2str(rho_1000), '_sobol_', num2str(sample), '_opt_', num2str(opt)], 'input_rho')
